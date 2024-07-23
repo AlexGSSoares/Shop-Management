6d215256c342e56932325541c7a7b7a2f6468d81
Alexandre Soares <168682727+AlexGSSoares@users.noreply.github.com>
2024-07-23 22:14:28 +0200

    Update store.pl

--- store.pl
+++ store.pl
@@ -1,13 +1,3 @@
-/**********************************************************
- ***************          EFOLIO B         ****************
- **********      Linguagens de Programação      ***********
- **********************************************************
- ***********    Professor: RICARDO BAPTISTA     ***********
- ***********   Monitor: RÚDI GUALTER OLIVEIRA   ***********
- ************     Aluno: Alexandre Soares      ************
- *************      Nr de Aluno: 2101521      *************
- *********************************************************/
-
 % Declaration Dynamic facts
 :- dynamic item/5.
 :- dynamic discount/2.
@@ -16,8 +6,8 @@
 :- dynamic client/4.
 :- dynamic history_purchase/7.
 
-% Item em inventário
-/*id_item, nome, categoria, custo, quantidade_inventário*/
+% Item in inventory
+/*id_item, name, category, cost, inventory_quantity*/
 item(1, 'Potion of Healing', 'potions', 10.0, 50).
 item(2, 'Wand of Fireball', 'wands', 20.0, 30).
 item(3, 'Enchanted Spellbook', 'enchanted_books', 30.0, 20).
@@ -31,10 +21,8 @@ item(10, 'Flying Broomstick', 'accessories', 50.0, 10).
 item(11, 'Enchanted Scroll', 'scrolls', 8.0, 50).
 item(12, 'Fairy Dust', 'ingredients', 5.0, 100).
 
-
-
-% Descontos por categoria de item
-/*categoria, desconto*/
+% Discounts by item category
+/*category, discount*/
 discount('potions', 0.1).
 discount('wands', 0.2).
 discount('enchanted_books', 0.3).
@@ -44,18 +32,17 @@ discount('accessories', 0.0).
 discount('scrolls', 0.2).
 discount('ingredients', 0.05).
 
-% Desconto de lealdade por ano
-/*ano_lealdade, desconto*/
+% Loyalty discount by year
+/*loyalty_year, discount*/
 loyalty_discount(1, 0.05).
 loyalty_discount(2, 0.1).
 loyalty_discount(3, 0.15).
 loyalty_discount(4, 0.2).
 loyalty_discount(5, 0.25).
-loyalty_discount(years, 0.30) :- years >5.
+loyalty_discount(years, 0.30) :- years > 5.
 
-
-% Custos de envio por distrito
-/*Distrito, custo*/
+% Shipping costs by district
+/*District, cost*/
 shipping_cost('Aveiro', 5.00).
 shipping_cost('Lisboa', 7.0).
 shipping_cost('Porto', 11.00).
@@ -64,9 +51,8 @@ shipping_cost('Coimbra', 5.0).
 shipping_cost('Faro', 15.0).
 shipping_cost('Viseu', 3.0).
 
-
-% Clientes
-/*id, nome, Distrito, anos_lealdade*/
+% Clients
+/*id, name, District, loyalty_years*/
 client(1, 'Alice', 'Aveiro', 3).
 client(2, 'Beatriz', 'Braga', 1).
 client(3, 'Carlos', 'Coimbra', 2).
@@ -75,8 +61,8 @@ client(5, 'Eva', 'Porto', 1).
 client(6, 'Francisca', 'Faro', 3).
 client(7, 'Guilhermina', 'Viseu', 5).
 
-% Histórico de Compras
-/* id_cliente, data, valor_sem_desconto, desconto_categoria, desconto_lealdade, custo_envio, total */
+% Purchase History
+/* client_id, date, pre_discount_amount, category_discount, loyalty_discount, shipping_cost, total */
 history_purchase(1, '20/05/2024', 50, 5, 0, 5, 50).
 history_purchase(2, '21/05/2024', 30, 3, 1, 3, 29).
 history_purchase(3, '22/05/2024', 40, 4, 0, 4, 40).
@@ -88,65 +74,63 @@ history_purchase(3, '27/05/2024', 45, 4.5, 0, 4.5, 45).
 history_purchase(4, '28/05/2024', 55, 5.5, 10, 5, 44.5).
 history_purchase(1, '28/05/2024', 60, 6, 0, 6, 60).
 
-
-
-
 /*
 *********************************************************
-********** Predicados ficheiro ManageInventory **********
+********** Predicates in file ManageInventory **********
 *********************************************************
 */
-% Predicados para visualizar itens e categorias
+
+% Predicates to view items and categories
 all_items(Items) :-
     findall([ID, Name, Category, Cost, Quantity], item(ID, Name, Category, Cost, Quantity), Items).
 
-% Items por categoria
+% Items by category
 items_by_category(Category, Items) :-
     findall([ID, Name, Category, Cost, Quantity], item(ID, Name, Category, Cost, Quantity), Items).
 
-% Predicado para listar todas as categorias disponíveis com base em descontos definidos
+% Predicate to list all available categories based on defined discounts
 available_categories(Categories) :-
     setof(Category, Discount^discount(Category, Discount), Categories).
 
-% Adicionar uma nova categoria de desconto
+% Add a new discount category
 add_discount(Category, Discount) :-
     \+ discount(Category, _),
     assertz(discount(Category, Discount)).
 
-% Modificar um desconto existente
+% Modify an existing discount
 modify_discount(Category, NewDiscount) :-
     retract(discount(Category, _)),
     assertz(discount(Category, NewDiscount)).
 
-% Remover uma categoria de desconto
+% Remove a discount category
 remove_discount(Category) :-
     retract(discount(Category, _)).
 
-% Adicionar um novo item
+% Add a new item
 add_item(Id, Name, Category, Cost, Quantity) :-
     \+ item(Id, _, _, _, _),
     assertz(item(Id, Name, Category, Cost, Quantity)).
 
-% Modificar um item existente
+% Modify an existing item
 modify_item(Id, Name, Category, Cost, Quantity) :-
     retract(item(Id, _, _, _, _)),
     assertz(item(Id, Name, Category, Cost, Quantity)).
 
-% Remover um item
+% Remove an item
 remove_item(Id) :-
     retract(item(Id, _, _, _, _)).
 
 /*
 *********************************************************
-*********** Predicados ficheiro ManageClients ***********
+*********** Predicates in file ManageClients ***********
 *********************************************************
 */
 
-% Listar todos os clientes
+% List all clients
 all_clients(Clients) :-
     findall([ID, Name, District, LoyaltyYears], client(ID, Name, District, LoyaltyYears), Clients).
 
-% Listar clientes por distrito
+% List clients by district
 clients_by_district(District, Clients) :-
     findall([ID, Name, District, LoyaltyYears], client(ID, Name, District, LoyaltyYears), Clients).
 
@@ -155,97 +139,97 @@ client_purchases(ClientID, Purchases) :-
             history_purchase(ClientID, Date, PreDiscountAmount, CategoryDiscount, LoyaltyDiscount, ShippingCost, Total),
             Purchases).
 
-% Listar clientes com lealdade acima de um certo valor
+% List clients with loyalty above a certain value
 clients_by_loyalty(MinLoyalty, Clients) :-
     findall([ID, Name, District, LoyaltyYears], (client(ID, Name, District, LoyaltyYears), LoyaltyYears > MinLoyalty), Clients).
 
-% Adicionar um novo cliente
+% Add a new client
 add_client(ID, Name, District, LoyaltyYears) :-
     \+ client(ID, _, _, _),
     assertz(client(ID, Name, District, LoyaltyYears)).
 
-% Modificar um cliente existente
+% Modify an existing client
 modify_client(ID, NewName, NewDistrict, NewLoyaltyYears) :-
     retract(client(ID, _, _, _)),
     assertz(client(ID, NewName, NewDistrict, NewLoyaltyYears)).
 
-% Remover um cliente
+% Remove a client
 remove_client(ID) :-
     retract(client(ID, _, _, _)).
 
 /*
 *********************************************************
-****** Predicados ficheiro ManageCostsandDiscounts ******
+****** Predicates in file ManageCostsandDiscounts ******
 *********************************************************
 */
 
-% Listar todos os custos de envio
+% List all shipping costs
 all_shipping_costs(Costs) :-
     findall([District, Cost], shipping_cost(District, Cost), Costs).
 
-% Adicionar um novo custo de envio
+% Add a new shipping cost
 add_shipping_cost(District, Cost) :-
     \+ shipping_cost(District, _),
     assertz(shipping_cost(District, Cost)).
 
-% Modificar um custo de envio existente
+% Modify an existing shipping cost
 modify_shipping_cost(District, NewCost) :-
     retract(shipping_cost(District, _)),
     assertz(shipping_cost(District, NewCost)).
 
-% Remover um custo de envio
+% Remove a shipping cost
 remove_shipping_cost(District) :-
     retract(shipping_cost(District, _)).
 
-% Listar todos os descontos de categoria
+% List all category discounts
 all_category_discounts(Discounts) :-
     findall([Category, Discount], discount(Category, Discount), Discounts).
 
-% Lista dos 5 primeiros descontos de lealdade + valor aritmético
+
+% List of the first 5 loyalty discounts + arithmetic value
 all_loyalty_discounts(Discounts) :-
      findall([Year, Discount], (between(1, 5, Year), loyalty_discount(Year, Discount)), TempDiscounts),
      append(TempDiscounts, [['>5', 0.30]], Discounts).
 
-% Adicionar um desconto de lealdade
+% Add a loyalty discount
 add_loyalty_discount(Years, Discount) :-
     \+ loyalty_discount(Years, _),
     assertz(loyalty_discount(Years, Discount)).
 
-% Modificar um desconto de lealdade existente
+% Modify an existing loyalty discount
 modify_loyalty_discount(Years, NewDiscount) :-
     retract(loyalty_discount(Years, _)),
     assertz(loyalty_discount(Years, NewDiscount)).
 
-% Remover um desconto de lealdade
+% Remove a loyalty discount
 remove_loyalty_discount(Years) :-
     retract(loyalty_discount(Years, _)).
 
 /*
 *********************************************************
-************ Predicados ficheiro SalesHistory ***********
+************ Predicates in file SalesHistory ***********
 *********************************************************
 */
 
-% Ver todas as vendas de uma determinada data:
+% View all sales on a specific date:
 sales_by_date(Date, Sales) :-
     findall([ClientID, Date, PreDiscountAmount, CategoryDiscount, LoyaltyDiscount, ShippingCost, Total],
             history_purchase(ClientID, Date, PreDiscountAmount, CategoryDiscount, LoyaltyDiscount, ShippingCost, Total),
             Sales).
 
-
-% Ver todas as vendas a um determinado cliente:
+% View all sales to a specific client:
 sales_by_client(ClientID, Sales) :-
     findall([ClientID, Date, PreDiscountAmount, CategoryDiscount, LoyaltyDiscount, ShippingCost, Total],
             history_purchase(ClientID, Date, PreDiscountAmount, CategoryDiscount, LoyaltyDiscount, ShippingCost, Total),
             Sales).
 
-% Ver os totais das vendas de um determinado distrito:
+% View total sales for a specific district:
 sales_by_district(District, Sales) :-
     findall([ClientID, Date, PreDiscountAmount, CategoryDiscount, LoyaltyDiscount, ShippingCost, Total],
             (client(ClientID, _, District, _), history_purchase(ClientID, Date, PreDiscountAmount, CategoryDiscount, LoyaltyDiscount, ShippingCost, Total)),
             Sales).
 
-% Ver os totais das vendas de um determinado distrito:
+% View total sales for a specific district:
 district_sales_totals(District, Totals) :-
     findall([PreDiscountAmount, CategoryDiscount, LoyaltyDiscount, ShippingCost, Total],
             (client(ClientID, _, District, _), history_purchase(ClientID, _, PreDiscountAmount, CategoryDiscount, LoyaltyDiscount, ShippingCost, Total)),
@@ -255,14 +239,14 @@ district_sales_totals(District, Totals) :-
 sum_sales([A,B,C,D,E], [F,G,H,I,J], [AF,BG,CH,DI,EJ]) :-
     AF is A+F, BG is B+G, CH is C+H, DI is D+I, EJ is E+J.
 
-% Ver os totais das vendas de uma determinada data:
+% View total sales for a specific date:
 date_sales_totals(Date, Totals) :-
     findall([PreDiscountAmount, CategoryDiscount, LoyaltyDiscount, ShippingCost, Total],
             history_purchase(_, Date, PreDiscountAmount, CategoryDiscount, LoyaltyDiscount, ShippingCost, Total),
             Sales),
     foldl(sum_sales, Sales, [0,0,0,0,0], Totals).
 
-% Calcula os totais de desconto (categoria + lealdade) por distrito
+% Calculate total discounts (category + loyalty) by district
 discounts_by_district(District, TotalDiscount) :-
     client(ClientID, _, District, _),
     findall(DiscountSum, 
@@ -271,21 +255,20 @@ discounts_by_district(District, TotalDiscount) :-
             Discounts),
     sum_list(Discounts, TotalDiscount).
 
-% Encontra o distrito com mais descontos
+% Find the district with the most discounts
 district_most_discounts(District) :-
     findall([Dist, TotalDiscount], discounts_by_district(Dist, TotalDiscount), Discounts),
     max_member([District, _], Discounts).
 
 /*
 *********************************************************
-**************** Predicados ficheiro Store **************
+**************** Predicates in file Store **************
 *********************************************************
 */
 
-% Atualiza o inventário ao diminuir a quantidade comprada
+% Update the inventory by decreasing the quantity sold
 update_inventory(ItemId, QuantitySold) :-
     item(ItemId, Name, Category, Cost, CurrentQuantity),
     NewQuantity is CurrentQuantity - QuantitySold,
     retract(item(ItemId, Name, Category, Cost, CurrentQuantity)),
     assertz(item(ItemId, Name, Category, Cost, NewQuantity)).
-
